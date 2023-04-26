286ff0577d0cd0939f3b1736d57d4c5f7ca6db55
Ekaanth <akanth1994@gmail.com>
2023-04-26 23:34:00 +0530

    Web3Auth connected

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,196 @@
+import { useEffect, useState } from "react";
+import { Web3Auth } from "@web3auth/modal";
+import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
+import PolkadotRPC from "./wallet/polkadotRPC";
 
-import Head from 'next/head';
-import dynamic from 'next/dynamic';
-import Image from 'next/image';
-
-const Connect = dynamic(() => import('../../components/Connnetct').then(m => m.Connect), {
-  ssr: false,
-});
+const clientId =
+  "BEglQSgt4cUWcj6SKRdu5QkOXTsePmMcusG5EAoyjyOYKlVRjIF1iCNnMOTfpzCiunHRrMui8TIwQPXdkQ8Yxuk"; // get from https://dashboard.web3auth.io
 
 
 export default function Home() {
+const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
+  const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(
+    null
+  );
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3auth = new Web3Auth({
+          clientId,
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.OTHER,
+          },
+          web3AuthNetwork: "cyan",
+        });
+        setWeb3auth(web3auth);
+
+        await web3auth.initModal();
+
+        if (web3auth.provider) {
+          setProvider(web3auth.provider);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    init();
+  }, []);
+
+  const login = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const web3authProvider = await web3auth.connect();
+    setProvider(web3authProvider);
+  };
+
+  const authenticateUser = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const idToken = await web3auth.authenticateUser();
+    uiConsole(idToken);
+  };
+
+  const getUserInfo = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const user = await web3auth.getUserInfo();
+    console.log(user);
+    uiConsole(user);
+  };
+
+  const logout = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    await web3auth.logout();
+    setProvider(null);
+  };
+
+  const onGetPolkadotKeypair = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new PolkadotRPC(provider as SafeEventEmitterProvider);
+    const polkadotKeypair = await rpc.getPolkadotKeyPair();
+    uiConsole("Keypair", polkadotKeypair);
+  };
+
+  const getAccounts = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new PolkadotRPC(provider);
+    const userAccount = await rpc.getAccounts();
+    uiConsole("Address", userAccount);
+  };
+
+  const getBalance = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new PolkadotRPC(provider);
+    const balance = await rpc.getBalance();
+    uiConsole("Balance", balance);
+  };
+
+  const signAndSendTransaction = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new PolkadotRPC(provider);
+    const result = await rpc.signAndSendTransaction();
+    uiConsole("Transaction ID: ", result);
+  };
+
+  function uiConsole(...args: any[]): void {
+    console.log(args);
+    const el = document.querySelector("#console>p");
+    if (el) {
+      el.innerHTML = JSON.stringify(args || {}, null, 2);
+    }
+  }
+
+  const loggedInView = (
+    <>
+      <div className="flex-container">
+        <div>
+          <button onClick={getUserInfo} className="card">
+            Get User Info
+          </button>
+        </div>
+        <div>
+          <button onClick={authenticateUser} className="card">
+            Get ID Token
+          </button>
+        </div>
+        <div>
+          <button onClick={onGetPolkadotKeypair} className="card">
+            Get Polkadot Keypair
+          </button>
+        </div>
+        <div>
+          <button onClick={getAccounts} className="card">
+            Get Accounts
+          </button>
+        </div>
+        <div>
+          <button onClick={getBalance} className="card">
+            Get Balance
+          </button>
+        </div>
+        <div>
+          <button onClick={signAndSendTransaction} className="card">
+            Send Transaction
+          </button>
+        </div>
+        <div>
+          <button onClick={logout} className="card">
+            Log Out
+          </button>
+        </div>
+      </div>
+      <div id="console" style={{ whiteSpace: "pre-line" }}>
+        <p style={{ whiteSpace: "pre-line" }}>Logged in Successfully!</p>
+      </div>
+    </>
+  );
+
+  const unloggedInView = (
+    <button onClick={login} className="card">
+      Login
+    </button>
+  );
+
+  const logInView = (
+    <button onClick={logout} className="card">
+      Logout
+    </button>
+  );
+
   return (
+    <>
     <header className="flex justify-between items-center px-4 py-3 bg-gray-900">
     <a href="#" className="text-white font-bold text-xl">Metaquity</a>
-  <Connect />
+    <div>
+    <div className="text-white bg-purple-600 hover:bg-purple-700 px-4 py-2 rounded-lg">{provider ? logInView : unloggedInView}</div>
+    </div>
   </header>
-  )
+  <div>
+    {provider ? loggedInView : null}
+  </div>
+    </>
+  );
 }
