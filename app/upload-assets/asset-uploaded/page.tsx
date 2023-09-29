5a9ad9add6445a5d756b072cca428074b1e42168
Ekaanth <akanth1994@gmail.com>
2023-09-29 20:39:10 +0530

    asset UI updated

--- app/upload-assets/asset-uploaded/page.tsx
+++ app/upload-assets/asset-uploaded/page.tsx
@@ -26,26 +26,6 @@ const assetUploaded: React.FC = () => {
           </div>
         </div>
       </div>
-
-      <div className="flex justify-center">
-        <div className="relative w-full lg:w-[70%] 2xsm:w-[100%] rounded-l py-12 px-8 text-center md:py-15 md:px-17.5">
-          <span className="mx-auto inline-block text-5xl text-primary">
-            <FaCheckCircle />
-          </span>
-          <h3 className="mt-5.5 pb-2 text-xl font-bold sm:text-4xl">Asset Uploaded Successfully!</h3>
-          <p className="pt-5">Please allow us a while to verify the asset and the associated licenses. </p>
-          <p className="mb-7.5"> We will notify you once the asset has been verified successfully, </p>
-          <p className="pt-8">Post verification, login and follow the steps to complete the asset onboarding.</p>
-          <div className="flex justify-center pt-15 ">
-            <button
-              className="flex flex-row md:w-[50%] 2xsm:w-[100%] h-10 py-2 justify-center rounded-full bg-primary hover:bg-opacity-90 p-3 font-medium text-gray gap-3"
-              onClick={() => router.push('/asset-verification')}
-            >
-              <div>Return To Home</div>
-            </button>
-          </div>
-        </div>
-      </div>
     </>
   );
 };
--- components/Charts/ChartSeven.tsx
+++ components/Charts/ChartSeven.tsx
@@ -1,7 +1,7 @@
 import { ApexOptions } from 'apexcharts';
 import React, { useState } from 'react';
-import dynamic from "next/dynamic";
-const ApexCharts = dynamic(() => import("react-apexcharts"), { ssr: false });
+import dynamic from 'next/dynamic';
+const ApexCharts = dynamic(() => import('react-apexcharts'), { ssr: false });
 
 interface ChartSevenState {
   series: {
@@ -111,7 +111,7 @@ const ChartSeven: React.FC = () => {
         title: {
           formatter: function (e) {
             return '';
-            e
+            e;
           },
         },
       },
@@ -121,20 +121,7 @@ const ChartSeven: React.FC = () => {
     },
     xaxis: {
       type: 'category',
-      categories: [
-        'Sep',
-        'Oct',
-        'Nov',
-        'Dec',
-        'Jan',
-        'Feb',
-        'Mar',
-        'Apr',
-        'May',
-        'Jun',
-        'Jul',
-        'Aug',
-      ],
+      categories: ['Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
       axisBorder: {
         show: false,
       },
@@ -155,14 +142,10 @@ const ChartSeven: React.FC = () => {
     <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pt-7.5 pb-5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-8">
       <div className="mb-6 flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-between">
         <div>
-          <h4 className="text-title-sm2 font-bold text-black dark:text-white">
-            Payments Overview
-          </h4>
+          <h4 className="text-title-sm2 font-bold text-black dark:text-white">Payments Overview</h4>
         </div>
         <div className="flex items-center">
-          <p className="font-medium uppercase text-black dark:text-white">
-            Short by:
-          </p>
+          <p className="font-medium uppercase text-black dark:text-white">Short by:</p>
           <div className="relative z-20 inline-block">
             <select
               name="#"
@@ -173,13 +156,7 @@ const ChartSeven: React.FC = () => {
               <option value="">Weekly</option>
             </select>
             <span className="absolute top-1/2 right-1 z-10 -translate-y-1/2">
-              <svg
-                width="18"
-                height="18"
-                viewBox="0 0 18 18"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+              <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M8.99995 12.8249C8.8312 12.8249 8.69058 12.7687 8.54995 12.6562L2.0812 6.2999C1.82808 6.04678 1.82808 5.65303 2.0812 5.3999C2.33433 5.14678 2.72808 5.14678 2.9812 5.3999L8.99995 11.278L15.0187 5.34365C15.2718 5.09053 15.6656 5.09053 15.9187 5.34365C16.1718 5.59678 16.1718 5.99053 15.9187 6.24365L9.44995 12.5999C9.30933 12.7405 9.1687 12.8249 8.99995 12.8249Z"
                   fill="#64748B"
@@ -191,27 +168,18 @@ const ChartSeven: React.FC = () => {
       </div>
       <div>
         <div id="chartSeven" className="-ml-5">
-          <ApexCharts
-            options={options}
-            series={state.series}
-            type="area"
-            height={310}
-          />
+          <ApexCharts options={options} series={state.series} type="area" height={310} />
         </div>
       </div>
 
       <div className="flex flex-col text-center xsm:flex-row">
         <div className="border-stroke py-2 dark:border-strokedark xsm:w-1/2 xsm:border-r">
           <p className="font-medium">Received Amount</p>
-          <h4 className="mt-1 text-title-sm font-bold text-black dark:text-white">
-            $45,070.00
-          </h4>
+          <h4 className="mt-1 text-title-sm font-bold text-black dark:text-white">$45,070.00</h4>
         </div>
         <div className="py-2 xsm:w-1/2">
           <p className="font-medium">Due Amount</p>
-          <h4 className="mt-1 text-title-sm font-bold text-black dark:text-white">
-            $32,400.00
-          </h4>
+          <h4 className="mt-1 text-title-sm font-bold text-black dark:text-white">$32,400.00</h4>
         </div>
       </div>
     </div>
